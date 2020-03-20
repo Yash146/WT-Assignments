716db53feda07704535a6b3780b411d48c80deb2
Yash146 <54269205+Yash146@users.noreply.github.com>
2020-03-20 21:54:48 +0530

    Add files via upload

---
+++ Insert.php
@@ -0,0 +1,33 @@
+<?php
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname ="assgn";
+$conn = new mysqli($host,$dbUsername,$dbPassword,$dbname);
+if($conn)
+{
+if(isset($_POST["btn"]))
+{
+$fulnme=$_POST['fname'];
+$ml=$_POST['mail'];
+$org=$_POST['org'];
+$cnt=$_POST['cntry'];
+$cntct=$_POST['num'];
+$ps=$_POST['pass'];
+$in="insert into reg values('$fulnme','$ml','$org','$cnt','$cntct','$ps') ";
+if(mysqli_query($conn,$in))
+{
+echo"<br><script>alert('entries are registered');</script><br>";
+}
+else
+{
+echo"Cant Register";
+}
+}
+else
+{
+  echo mysqli_error($conn);
+}
+}
+mysqli_close($conn);
+?>
